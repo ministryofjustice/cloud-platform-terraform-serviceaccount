869b8351a9c2ef478953e5f85a784ae4fb1e4e53
David Salgado <david@digitalronin.com>
2021-01-28 16:26:37 +0900

    Provision serviceaccount & github actions secrets
    This code creates a serviceaccount with the 
    permissions required to run a CI/CD pipeline and,
    if a list of github repositories is supplied, it
    creates github actions secrets containing the
    serviceaccount ca.crt and token, to enable the
    github action to deploy to the cluster.
    
    This requires a TF_VAR_github_token able to manage
    github actions secrets in the target repositories

---
+++ main.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_service_account" "github_actions_serviceaccount" {
+  metadata {
+    name      = var.serviceaccount_name
+    namespace = var.namespace
+  }
+}
+
+data "kubernetes_secret" "github_actions_secret" {
+  metadata {
+    name      = kubernetes_service_account.github_actions_serviceaccount.default_secret_name
+    namespace = var.namespace
+  }
+}
+
+resource "kubernetes_role" "github_actions_role" {
+  metadata {
+    name      = "github-actions-role"
+    namespace = var.namespace
+  }
+
+  dynamic rule {
+    for_each = var.serviceaccount_rules
+    content {
+      api_groups = rule.value.api_groups
+      resources  = rule.value.resources
+      verbs      = rule.value.verbs
+    }
+  }
+}
+
+resource "kubernetes_role_binding" "github-actions-rolebinding" {
+  metadata {
+    name      = "github-actions-rolebinding"
+    namespace = var.namespace
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.github_actions_role.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.github_actions_serviceaccount.metadata[0].name
+    namespace = var.namespace
+  }
+}
+
+resource "github_actions_secret" "serviceaccount-cert" {
+  for_each        = toset(var.github_repositories)
+  repository      = each.key
+  secret_name     = var.github_actions_secret_kube_cert
+  plaintext_value = lookup(data.kubernetes_secret.github_actions_secret.data, "ca.crt")
+}
+
+resource "github_actions_secret" "serviceaccount-token" {
+  for_each        = toset(var.github_repositories)
+  repository      = each.key
+  secret_name     = var.github_actions_secret_kube_token
+  plaintext_value = lookup(data.kubernetes_secret.github_actions_secret.data, "token")
+}
---
+++ variables.tf
@@ -0,0 +1,76 @@
+variable "namespace" {
+  description = "The namespace in which this serviceaccount will be created"
+}
+
+variable "serviceaccount_name" {
+  description = "The name of the serviceaccount"
+  default     = "cd-serviceaccount"
+}
+
+variable "serviceaccount_rules" {
+  description = "The capabilities of this serviceaccount"
+
+  type = list(object({
+    api_groups = list(string),
+    resources  = list(string),
+    verbs      = list(string)
+  }))
+
+  # These values are usually sufficient for a CI/CD pipeline
+  default = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "delete",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "networking.k8s.io",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+  ]
+}
+
+variable "github_repositories" {
+  description = "GitHub repositories in which to create github actions secrets"
+  type        = list(string)
+  default     = []
+}
+
+variable "github_actions_secret_kube_cert" {
+  description = "The name of the github actions secret containing the serviceaccount ca.crt"
+  default     = "KUBE_CERT"
+}
+
+variable "github_actions_secret_kube_token" {
+  description = "The name of the github actions secret containing the serviceaccount token"
+  default     = "KUBE_TOKEN"
+}
