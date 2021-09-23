7cf393c31c2ab00352905d47d2f90e40375bee70
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-09-23 08:34:09 +0100

    pin

--- versions.tf
+++ versions.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     github = {
       source = "integrations/github"
+      version = "~> 4.14.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 }
