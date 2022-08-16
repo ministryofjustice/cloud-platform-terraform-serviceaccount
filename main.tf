e2a374f07022fba50f2518e99764cb96d1bcb66d
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-08-16 10:27:01 +0100

    Create sa secrets environment secrets (#15)
    * Create sa secrets environment secrets
    
    Related to:
    https://github.com/ministryofjustice/cloud-platform/issues/4039
    
    * Add documentation and unit test github workflows

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+locals {
+  github_repositories = toset([
+    for repository in var.github_repositories : {
+      repository = repository
+    }
+  ])
+
+  github_environments = toset([
+    for environment in var.github_environments : {
+      environment = environment
+    }
+  ])
+
+  github_repo_environments = [
+    for pair in setproduct(local.github_repositories, local.github_environments) : {
+      repository  = pair[0].repository
+      environment = pair[1].environment
+
+    }
+  ]
+}
+
 resource "kubernetes_service_account" "github_actions_serviceaccount" {
   metadata {
     name      = var.serviceaccount_name
@@ -18,7 +40,7 @@ resource "kubernetes_role" "github_actions_role" {
     namespace = var.namespace
   }
 
-  dynamic rule {
+  dynamic "rule" {
     for_each = var.serviceaccount_rules
     content {
       api_groups = rule.value.api_groups
@@ -69,6 +91,48 @@ resource "github_actions_secret" "cluster-name" {
 resource "github_actions_secret" "cluster-namespace" {
   for_each        = toset(var.github_repositories)
   repository      = each.key
+  secret_name     = var.github_actions_secret_kube_namespace
+  plaintext_value = var.namespace
+}
+
+# Create environment secrets
+
+resource "github_actions_environment_secret" "serviceaccount-cert" {
+  for_each = {
+    for i in local.github_repo_environments : "${i.repository}.${i.environment}" => i
+  }
+  repository      = each.value.repository
+  environment     = each.value.environment
+  secret_name     = var.github_actions_secret_kube_cert
+  plaintext_value = lookup(data.kubernetes_secret.github_actions_secret.data, "ca.crt")
+}
+
+resource "github_actions_environment_secret" "serviceaccount-token" {
+  for_each = {
+    for i in local.github_repo_environments : "${i.repository}.${i.environment}" => i
+  }
+  repository      = each.value.repository
+  environment     = each.value.environment
+  secret_name     = var.github_actions_secret_kube_token
+  plaintext_value = lookup(data.kubernetes_secret.github_actions_secret.data, "token")
+}
+
+resource "github_actions_environment_secret" "cluster-name" {
+  for_each = {
+    for i in local.github_repo_environments : "${i.repository}.${i.environment}" => i
+  }
+  repository      = each.value.repository
+  environment     = each.value.environment
+  secret_name     = var.github_actions_secret_kube_cluster
+  plaintext_value = var.kubernetes_cluster
+}
+
+resource "github_actions_environment_secret" "cluster-namespace" {
+  for_each = {
+    for i in local.github_repo_environments : "${i.repository}.${i.environment}" => i
+  }
+  repository      = each.value.repository
+  environment     = each.value.environment
   secret_name     = var.github_actions_secret_kube_namespace
   plaintext_value = var.namespace
 }
--- variables.tf
+++ variables.tf
@@ -96,6 +96,12 @@ variable "github_repositories" {
   default     = []
 }
 
+variable "github_environments" {
+  description = "GitHub environment in which to create github actions secrets"
+  type        = list(string)
+  default     = []
+}
+
 variable "github_actions_secret_kube_cluster" {
   description = "The name of the github actions secret containing the kubernetes cluster name"
   default     = "KUBE_CLUSTER"
