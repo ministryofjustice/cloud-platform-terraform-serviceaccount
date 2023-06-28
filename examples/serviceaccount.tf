c9b2f1c0811f3e63867e82668126086699c0c4dc
jakemulley <jakemulley@users.noreply.github.com>
2023-06-28 17:12:26 +0100

    Fix failing test

--- examples/serviceaccount.tf
+++ examples/serviceaccount.tf
@@ -9,6 +9,7 @@ module "serviceaccount" {
 
   namespace           = "my-namespace"
   github_repositories = ["my-repo"]
+  kubernetes_cluster  = "live-1"
 
   # list of github environments, to create the service account secrets as environment secrets
   # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
