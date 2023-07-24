2cd8b01fb21b8a88bee0cf5785ad5fddbb55fd49
sj-williams <steve.williams@digital.justice.gov.uk>
2023-07-24 13:24:18 +0100

    update service account secret name output reference

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "service_account" {
 
 output "default_secret_name" {
   description = "Name of the Kubernetes secret containing the service account's credentials"
-  value       = kubernetes_service_account.github_actions_serviceaccount.default_secret_name
+  value       = kubernetes_secret_v1.serviceaccount-token.metadata[0].name
 }
